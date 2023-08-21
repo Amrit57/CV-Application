0687bdf116fb2edf56a3a4268d555a7be37dd0d8
Amrit57 <fenpadorje7@gmail.com>
2023-08-21 09:17:26 +0545

    Refactored classname and style of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Personal from "./components/Personal";
+import Form from "./components/Form";
 import CV from "./components/CV"
 
 export default function App (){
     return(
         <div className="app--container">
             <div className="left-side"> 
-                <Personal/> 
+                <Form/> 
             </div>
             <div className="right-side">
                 <CV/>
