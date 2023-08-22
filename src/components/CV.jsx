d4e9ce1e728629ec290e68695defe868884ff25a
Amrit57 <fenpadorje7@gmail.com>
2023-08-22 09:52:23 +0545

    Created a mockup styles to the CV preview.

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,44 +1,75 @@
 import React from "react";
 export default function CV() {
   return (
-    <div className="cv-container">
-      <header>
+    <div className="cv--container">
+      <header className="cv--header">
         <h1>Amrit Lama Syangbo</h1>
+        <div className="cv--contact" >
         <p className="cv--email">abcd@gmail.com</p>
         <p className="cv--phoneNumber">12478498616</p>
         <p className="cv--address">JeetpurSimara-22, Nepal</p>
+        </div>
       </header>
-      <main>
-        <section className="cv--personal">
+      <main className="cv--main">
+        <section className="cv--education">
           <h2 className="cv--header-text">Education</h2>
+          <div className="cv--education-detail">
           <div>
             <h4 className="cv--collegeName">Hetauda Campus</h4>
-            <p className="duration">2016/4/6 - 2018/11/30</p>
+            <span className="cv--duration">2016/4/6 - 2018/11/30</span>
           </div>
           <div>
             <h4 className="cv--degreeName">Bachelor in Computer Science</h4>
           </div>
-        </section>
-        <section className="cv--experience">
+            </div>
+            <div className="cv--education-detail">
           <div>
-            <h4 className="cv--companyName">Xyz Company</h4>
-            <h4 className="cv--position">
-              <i>Front-End Developer</i>
-            </h4>
-            <p className="cv--duration">2018/4/7 - 2022/10/8</p>
+            <h4 className="cv--collegeName">Hetauda Campus</h4>
+            <span className="cv--duration">2016/4/6 - 2018/11/30</span>
           </div>
           <div>
-            <p className="job-description">
+            <h4 className="cv--degreeName">Bachelor in Computer Science</h4>
+          </div>
+            </div>
+            
+        </section>
+        <section className="cv--experience">
+          <h2 className="cv--header-text">Experience</h2>
+          <div className="cv--experience-detail">
+
+          <div>
+            <h4 className="cv--companyName">Xyz Company</h4>
+            <span className="cv--duration">2018/4/7 - 2022/10/8</span>
+          </div>
+          <div>
+            <h4 className="cv--position">
+              Front-End Developer
+            </h4>
+            <p className="cv--job-description">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Perspiciatis possimus ducimus esse nesciunt, omnis temporibus
               earum, necessitatibus sunt culpa obcaecati nostrum nemo nihil
               suscipit, fuga facilis eveniet? Soluta expedita asperiores eum
-              atque earum alias officia nobis voluptatum, iure inventore
-              perferendis consequatur voluptas rerum labore, doloribus minima
-              necessitatibus non, ducimus dolorem cumque amet obcaecati. Eius
-              sequi nisi a repellendus reiciendis necessitatibus!
             </p>
           </div>
+          </div>
+          <div className="cv--experience-detail">
+
+<div>
+  <h4 className="cv--companyName">Xyz ABc Efgh  aldsjlaksn Company</h4>
+  <span className="cv--duration">2018/4/7 - 2022/10/8</span>
+</div>
+<div>
+  <h4 className="cv--position">
+    Front-End Developer
+  </h4>
+  <p className="cv--job-description">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    Perspiciatis possimus ducimus esse nesciunt, omnis temporibus
+
+  </p>
+</div>
+</div>
         </section>
       </main>
     </div>
