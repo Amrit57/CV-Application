e93d09e9a1a90caa85b1f909db43265a51843c7b
Amrit57 <fenpadorje7@gmail.com>
2023-09-24 21:31:34 +0545

    Added clear functionality to button.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar({ clearResume, downloadResume }) {
   return (
     <nav className="navbar">
-        <h1 className="navbar--heading">
-            CV Builder
-        </h1>
+      <h1 className="navbar--heading">CV Builder</h1>
       <div className="navbar--btn-container">
-        <button className="navbar--btn" type="button">
+        <button className="navbar--btn" onClick={clearResume}>
           Clear Resume
-        </button>
-        <button className="navbar--btn" type="button">
-         Download
         </button>
       </div>
     </nav>
