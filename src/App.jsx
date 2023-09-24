fbaa71c572bb4d8a2492a578087b1748cec3a9bc
Amrit57 <fenpadorje7@gmail.com>
2023-09-24 21:29:55 +0545

    Tranfered Navbar inside Form component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 export default function App() {
   return (
     <div>
-      <Navbar />
       <Form />
     </div>
   );
