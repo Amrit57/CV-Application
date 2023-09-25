d1014b3f14ea68745cf65057eed1822bb771d6f4
Amrit57 <fenpadorje7@gmail.com>
2023-09-25 13:08:08 +0545

    Added Load example button to load mock CV.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-export default function Navbar({ clearResume, downloadResume }) {
+export default function Navbar({ clearResume, loadResume }) {
   return (
     <nav className="navbar">
       <h1 className="navbar--heading">CV Builder</h1>
       <div className="navbar--btn-container">
         <button className="navbar--btn" onClick={clearResume}>
           Clear Resume
+        </button>
+        <button className="navbar--btn" onClick={loadResume}>
+          Load Example
         </button>
       </div>
     </nav>
