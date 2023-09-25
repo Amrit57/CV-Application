5e6db2b1c4847df63f990e63dfd17c79c1621c1e
Amrit57 <fenpadorje7@gmail.com>
2023-09-25 14:53:52 +0545

    Added a link to github repo.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,18 @@ import React from "react";
 export default function Navbar({ clearResume, loadResume }) {
   return (
     <nav className="navbar">
-      <h1 className="navbar--heading">CV Builder</h1>
-      <div className="navbar--btn-container">
+      <div>
+        <h1 className="navbar--heading">CV Builder</h1>
+        <span>By </span>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Amrit57/CV-Application"
+        >
+          Amrit Lama Syangbo
+        </a>
+      </div>
+      <div className="navbar--tab-container">
         <button className="navbar--btn" onClick={clearResume}>
           Clear Resume
         </button>
